00e4c14bca7ded8ef90cf18642dff4322d30e8ed
Emmaculate <emmamwitimi14@gmail.com>
2024-09-10 07:52:31 +0300

    complete lab

---
+++ lib/insert.sql
@@ -0,0 +1,9 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES
+('Mr. Chocolate', 10, 'M', 'Brown', 'Calm', 1),
+('Rowdy', 5, 'M', 'Black', 'Aggressive', 1),
+('Tabitha', 3, 'F', 'Brown', 'Shy', 1),
+('Sergeant Brown', 7, 'M', 'Brown', 'Bold', 1),
+('Melissa', 4, 'F', 'Black', 'Friendly', 1),
+('Grinch', 6, 'M', 'Green', 'Grumpy', 1),
+('Wendy', 2, 'F', 'White', 'Playful', 1),
+(NULL, 1, 'F', 'Brown', 'Curious', 1);
